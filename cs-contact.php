b30a6ff8161b3cc83bc0d655ff63594bc623d980
Korawich <nay365117@gmail.com>
2024-09-27 11:21:33 +0700

    Initial commit

---
+++ cs-contact.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>หลักสูตรวิทยาการคอมพิวเตอร์</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta property="og:url" content="http://cs.vru.ac.th" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="หลักสูตรวิทยาการคอมพิวเตอร์ มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ " />
+    <meta property="og:description" content="หลักสูตรวิทยาการคอมพิวเตอร์ คณะวิทยาศาสตร์และเทคโนโลยี มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์ " />
+    <meta property="og:image" content="http://cs.vru.ac.th/images/logo5.png" />
+    
+
+    
+    <link rel="icon" href="http://cs.vru.ac.th/images/css.png">
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
+</head>
+
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+    <div class="site-wrap">
+        <div class="site-mobile-menu site-navbar-target">
+            <div class="site-mobile-menu-header">
+                <div class="site-mobile-menu-close mt-3">
+                    <span class="icon-close2 js-menu-toggle"></span>
+                </div>
+                <img class="m-4" src="http://cs.vru.ac.th/images/css.png" height="70">
+            </div>
+            <div class="site-mobile-menu-body"></div>
+        </div>
+    </div>
+    
+
+
+    <!-- start of content , see end in footer -->
+    <div class="content">
+        <div class="container">
+            <br />
+            <h2>&starf;&nbsp;ติดต่อเรา</h2>
+            <hr>
+
+            <style>
+                #map_container {
+                    height: 250px;
+                    margin-bottom: 10px;
+                }
+
+                #map {
+                    width: 100%;
+                    height: 100%;
+                }
+            </style>
+            <div id="map_container">
+                <div id="map"></div>
+            </div>
+            
+
+            
+
+            <p></p>
+            <h4>หลักสูตรวิทยาการคอมพิวเตอร์</h4>
+            <p>&nbsp; &nbsp; &nbsp; &nbsp;อาคาร 75 ปี วไลยอลงกรณ์ </p>
+            <p>&nbsp; &nbsp; &nbsp; &nbsp;มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์</p>
+            <p>&nbsp; &nbsp; &nbsp; &nbsp;เลขที่ 1 หมู่ 20 ถนนพหลโยธิน กม. 48 ต.คลองหนึ่ง อ.คลองหลวง จ.ปทุมธานี 13180</p>
+            <p>&nbsp; &nbsp; &nbsp; &nbsp;โทรศัพท์ 0-2529-0674-7 ต่อ 365 โทรสาร 02-909-3029</p>
+            &nbsp; &nbsp; &nbsp; &nbsp;<a href="https://www.facebook.com/profile.php?id=100008113098809&fref=ts"><img src="http://cs.vru.ac.th/images/facebook-icon-preview-1.png" width="50" height="50"> FACEBOOK หลักสูตรวิทยาการคอมพิวเตอร์</a>
+        </div>
+    </div>
+    <!-- end of content, see start in header -->
+    <style>
+        .site-footer,
+        .footer-heading,
+        .site-footer .p {
+            font-size: 16px;
+            text-align: center !important;
+            background: #8e24aa !important;
+            /*color: #fff!important;*/
+        }
+
+        .site-footer p,
+        .site-footer a {
+            color: #bbb !important;
+        }
+
+        .site-footer .border-top {
+            border-top: 1px solid #bbb !important;
+        }
+
+        .site-footer {
+            padding: 0 !important;
+            padding-top: 4em !important;
+            padding-bottom: 4em !important;
+
+        }
+    </style>
+    <div class="container-fluid bg-light  p-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 pl-4">
+                    <h3>สมัครเรียนกับเรา</h3>
+                    <h5 id="schedule">สอบถามข้อมูลการรับสมัครเพิ่มเติม</h5>
+                    <p>โทร. 02-909-1432 ต่อ 322 หรือ 323</p>
+                </div>
+                <div class="col-md-6 text-right">
+                    <div class="py-4">
+                        <a class="btn btn-primary" href="tcas.php">รายะละเอียดสมัครเรียน</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+    
+
+
+
+
+
+</body>
+
+</html>
---
+++ layouts/cs-header.php
@@ -0,0 +1,38 @@
+<header class="site-navbar js-sticky-header site-navbar-target justify-content-center" role="banner">
+    <div class="container ">
+        <div class="row">
+            <div class="col-10 col-md-10 col-lg-10 col-xl-4">
+                <div class="site-logo">
+                    <a href="index.php" class="text-left">
+                        <div class="" style="display: flex; flex-direction:row;">
+                            <img class="d-lg-block d-md-none d-sm-none d-none my-1" src="http://cs.vru.ac.th/images/css.png" height="70">
+                            <div class="d-lg-block" style="line-height: 0.3;   padding-top: 18px;     padding-left: 10px;">
+                                <h1 style="margin-bottom:0px; font-size: 0.8em;" class="text-dark">วิทยาการคอมพิวเตอร์</h1>
+                                <small class="" style="font-size: 0.35em; color:#777;">มหาวิทยาลัยราชภัฏวไลยอลงกรณ์ ในพระบรมราชูปถัมภ์</small>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="d-none d-md-none col-lg-8 d-xl-block">
+                <nav class="site-navigation position-relative text-right" role="navigation">
+                    <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                        <li class="nav-link"><a class="nav-link" href="index.php">หน้าหลัก</a></li>
+                        <li class="nav-link dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="aboutDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">เกี่ยวกับหลักสูตร</a>
+                            <div class="dropdown-menu" aria-labelledby="aboutDropdown">
+                                <a class="dropdown-item" href="history.php">ประวัติหลักสูตร</a>
+                                <a class="dropdown-item" href="structure.php">โครงสร้างหลักสูตร</a>
+                                <!-- ... อื่นๆ ... -->
+                            </div>
+                        </li>
+                        <!-- ... ส่วนอื่นๆ ของเมนู ... -->
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-2 col-md-2 d-l-block d-inline-block d-xl-none ml-md-0 py-3">
+                <a href="#" class="site-menu-toggle js-menu-toggle text-dark float-right"><span class="icon-menu h3"></span></a>
+            </div>
+        </div>
+    </div>
+</header>
